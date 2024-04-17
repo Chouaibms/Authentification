3897505f1932cfd50ae079022a6d97014ff3fc7c
Chouaib Massa <153142520+Chouaibms@users.noreply.github.com>
2024-04-18 00:48:36 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Authen from './Authen'
+export default function App() {
+  return (
+    <section>
+      <Authen/>
+    </section>
+  )
+}
---
+++ src/Authen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Ofppt from './assets/ofppt-logo-png.png'
+export default function Authen() {
+  return (
+    <section className='w-100'>
+      <div className='row w-100 d-flex justify-content-center pt-5'>
+        <img src={Ofppt} alt='ofppt' style={{ width: '25%' }} />
+      </div>
+      <div className='pt-5'>
+        <form>
+          <div className='row row w-100 d-flex justify-content-center mb-4'>
+            <div className='col-5 text-end my-auto'>
+              Login
+            </div>
+            <div className='col-5'>
+              <input className='form-control w-50' placeholder='Enter Your Login' />
+            </div>
+          </div>
+          <div className='row row w-100 d-flex justify-content-center mb-4'>
+            <div className='col-5 text-end my-auto'>
+              Password
+            </div>
+            <div className='col-5'>
+              <input type='password' className='form-control w-50' placeholder='Enter Your Password' />
+            </div>
+          </div>
+          <div className='row row w-100 d-flex justify-content-center mb-4'>
+            <div className='col-5 text-end my-auto'>
+              &nbsp;
+            </div>
+            <div className='col-5'>
+            <button className='btn btn-primary' type='submit'>Login</button> <br/>
+            <small><u>Forgot your password ?</u></small>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
